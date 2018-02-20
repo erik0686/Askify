26cb5795074ef56b711236f86a41f6c2d4cb48d7
Cristy Jimenez <cristinajimenezcanales@gmail.com>
2018-02-19 22:41:11 -0600

    controller answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,44 @@
-class AnswersController < ApplicationController
+class AnswerController < ApplicationController
+
+def index
+    @answers = Answer.all
+end
+
+def new
+    @answers = Answer.new
+end
+
+def create
+    @answers = Answer.new(answer_params)
+    if @answers.save
+        redirect_to answer_path
+    else
+        render 'new'
+    end
+end
+
+def edit
+    @answers = Answer.find(params[:id])
+end
+
+def update
+    @answers = Answer.find(params[:id])
+    if @answers.update(answers_params)
+        redirect_to answers_path
+    else
+        render :edit
+    end
+end
+
+def destroy
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to answers_path
+end
+
+    private
+
+    def answer_params
+        params.require(:answer).permit(:answer_text)
+    end
 end
