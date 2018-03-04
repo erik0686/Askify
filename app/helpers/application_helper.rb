1a90ff6494e63e23bcda9dda2daa50edc095dc26
pauescalantec <pauescalante8@hotmail.com>
2018-03-04 14:27:04 -0600

    View for profile now works, edit and save changes, only view, no functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,24 @@
 module ApplicationHelper
 
     def profileCardMaker(title, data)
-        data = '<p class="lead">
-        <h5>' + title + '</h5>
-        <div class="card">
+        data = '<p class="lead showing">
+        <h5 class="showing">' + title + '</h5>
+        <div class="card showing">
             <div class="card-body">' + data + '</div>
+        </div>
+        <p>'
+        data.html_safe
+    end
+
+    def profileEditableCardMaker(title, data)
+        data = '<p class="lead hidden">
+        <h5 class="hidden">' + title + '</h5>
+        <div class="card hidden">
+            <div class="card-body">
+                <div class="form-group editProfile">
+                    <input type="text" class="form-control editProfile" value="' + data +'"placeholder="First Name">
+                </div>
+            </div>
         </div>
         <p>'
         data.html_safe
