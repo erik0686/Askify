dd1d807f9b9107f2d34b1c8a3a9daa47f4563cf9
erik0686 <erik.torres.aguilar@gmail.com>
2018-03-04 09:25:37 -0600

    index page

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,9 @@
 class Answer < ApplicationRecord
-	belongs_to :question
-	belongs_to :user
-	has_many :comments
-	has_many :users, through: :answer_rating
+  belongs_to :question
+  #belongs_to :user
+  has_many :comments
+  has_many :answer_rating
+  has_many :users, through: :answer_rating
 
-	validates :answer_text, presence: true
-	validates_associated :question
+  validates :answer_text, presence: true
 end
