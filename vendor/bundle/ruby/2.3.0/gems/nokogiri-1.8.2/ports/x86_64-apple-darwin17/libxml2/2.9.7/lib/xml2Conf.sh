e564f6ef0106b5cbb9cc470c3c8dbf9219fdbc69
Cristy Jimenez <cristinajimenezcanales@gmail.com>
2018-04-02 16:39:28 -0500

    deletes vendor folder

--- vendor/bundle/ruby/2.3.0/gems/nokogiri-1.8.2/ports/x86_64-apple-darwin17/libxml2/2.9.7/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/kiki/Desktop/Askify/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.8.2/ports/x86_64-apple-darwin17/libxml2/2.9.7/lib"
-XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/kiki/Desktop/Askify/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.8.2/ports/x86_64-apple-darwin17/libxml2/2.9.7/include/libxml2"
-MODULE_VERSION="xml2-2.9.7"
-
