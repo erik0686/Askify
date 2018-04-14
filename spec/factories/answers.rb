4a37de799d771d488fcaa7a54c9af85764c9f33a
pauescalantec <pauescalante8@hotmail.com>
2018-04-14 14:36:54 -0500

    fixed search css

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :answer do
-    answer_text "Esta es la respuesta a una pregunta hecha por mi, deberia ser lo suficientemente larga para poder abarcar un gran espacio"
-    user_id 5
-    question_id 19
+    answer_text "Esta es la respuesta a una pregunta, deberia ser lo suficientemente larga para poder abarcar un gran espacio"
+    user_id 1
+    question_id 2
   end
 end
