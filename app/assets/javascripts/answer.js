e34d7a0b77f67ce5859a16bfa3b46a6bf1d1c946
pauescalantec <pauescalante8@hotmail.com>
2018-04-15 18:16:32 -0500

    fix display none

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -1,8 +1,6 @@
 $(document).on('turbolinks:load', function () {
-
   $(".answer-question").click(function() {
     $(".answer-form").removeClass("hidden");
-    $(".answer-question").addClass("hidden");
     $(".answer-question").addClass("hidden");
     event.preventDefault();
   });
