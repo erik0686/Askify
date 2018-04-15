4bf3d3f40ce729e29c1025793b79df59524f3706
pauescalantec <pauescalante8@hotmail.com>
2018-04-15 17:48:48 -0500

    submit answer works and view question with answers is pretty now!

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -1,7 +1,10 @@
 $(document).on('turbolinks:load', function () {
+
   $(".answer-question").click(function() {
     $(".answer-form").removeClass("hidden");
+    $(".answer-question").addClass("hidden");
     $(".answer-question").addClass("hidden");
     event.preventDefault();
   });
 });
+
