e39d6946676920f2ab25ecde69950299dd552822
pauescalantec <pauescalante8@hotmail.com>
2018-04-17 07:19:34 -0500

    default img

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,6 @@ class User < ApplicationRecord
 
   validates :student_id, :email, :user_name, uniqueness: true
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "userImage.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
