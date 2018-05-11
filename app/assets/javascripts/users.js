34b6850d026267970eca6525060f5200d1d67b8f
Erik Torres <erik.torres.aguilar@gmail.com>
2018-05-11 17:11:08 -0500

    little tweaks

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -154,15 +154,6 @@ function reloadUserPage(){
     $(this).parent().parent().parent()
     .removeClass("cardWithFocus");
   });
-
-  /*$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-    });*/
 }
 
 function saveData(){
